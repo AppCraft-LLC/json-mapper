c01a1309149633ffb9747c53f13075cee52dec61
Michael Ovchinnikov <michael@Mac-mini-Michael.local>
2016-10-15 14:19:54 +0300

    Initial Commit

---
+++ JSONMapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSONMapper
+//
+//  Created by Michael Ovchinnikov on 15/10/2016.
+//  Copyright © 2016 Michael Ovchinnikov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSONMapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONMapper
+//
+//  Created by Michael Ovchinnikov on 15/10/2016.
+//  Copyright © 2016 Michael Ovchinnikov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
